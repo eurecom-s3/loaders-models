bb55add5b7caf4906a071ce19b2d94e1be1efb96
Dario Nisi <dario.nisi@eurecom.fr>
2020-03-05 11:27:26 +0100

    Glibc model compile

--- linux/glibc_extra.h
+++ linux/glibc_extra.h
@@ -1,7 +1,8 @@
+typedef uint32_t Elf32_Word;
 typedef struct elf32_note {
     Elf32_Word namesz;
     Elf32_Word descsz;
     Elf32_Word type;
     char name[4];
-    Elf32_Word desc[];
-} Elf32_Note;  // TODO: hard-coded namesz for GNU\0
+    Elf32_Word desc[40];
+} Elf32_Note;
